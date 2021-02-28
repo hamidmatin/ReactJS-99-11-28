3b489d9f30e48b32fb909daa62e18a77302bee3b
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2021-02-28 20:12:42 +0330

    update

--- src/components/students/Students.js
+++ src/components/students/Students.js
@@ -3,6 +3,29 @@ import Student from './Student';
 import './Students.css';
 
 export default class Students extends Component {
+  constructor() {
+    super();
+    this.state = {
+      studentList: [
+        {
+          FirstName: 'Mohammad Mehdi',
+          lastName: 'Amiri',
+        },
+        {
+          FirstName: 'Pejman',
+          lastName: 'Namdari',
+        },
+        {
+          FirstName: 'Mohammad Mehdi',
+          lastName: 'Moallemi',
+        },
+        {
+          FirstName: 'Negin',
+          lastName: 'Salehi',
+        },
+      ],
+    };
+  }
   render() {
     return (
       <div className='container'>
@@ -10,10 +33,22 @@ export default class Students extends Component {
           <span>Student List</span>
         </h2>
         <div className='student-list'>
-          <Student FirstName='Mohammad Mehdi' LastName='Amiri' />
-          <Student FirstName='Pejman' LastName='Namdari' />
-          <Student FirstName='Mohammad Mehdi' LastName='Moallemi' />
-          <Student FirstName='Negin' LastName='Salehi' />
+          <Student
+            FirstName={this.state.studentList[0].FirstName}
+            LastName={this.state.studentList[0].lastName}
+          />
+          <Student
+            FirstName={this.state.studentList[1].FirstName}
+            LastName={this.state.studentList[1].lastName}
+          />
+          <Student
+            FirstName={this.state.studentList[2].FirstName}
+            LastName={this.state.studentList[2].lastName}
+          />
+          <Student
+            FirstName={this.state.studentList[3].FirstName}
+            LastName={this.state.studentList[3].lastName}
+          />
         </div>
       </div>
     );

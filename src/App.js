10661579085096ac04fbb75d2b02c5387ae728a6
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2021-03-09 20:04:35 +0330

    Create admin route and layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // import object from 'module'
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
+import AdminLayout from './containers/layout/AdminLayout';
 import MainLayout from './containers/layout/MainLayout';
 class App extends Component {
-  
   render() {
-    return <MainLayout />;
+    return (
+      <BrowserRouter>
+        <Route path='/' exact component={MainLayout} />
+        <Route path='/admin'  component={AdminLayout} />
+      </BrowserRouter>
+    );
   }
 }
 export default App;
---
+++ src/containers/layout/AdminLayout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AdminLayout extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Admin</h2>
+      </div>
+    )
+  }
+}

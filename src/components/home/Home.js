47540c9e2ac5f0de58c53c81c92c731679a25453
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2021-04-11 19:13:21 +0430

    Clouser

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,66 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className={['container', 'mt-5'].join(' ')}>
-        <h3 className='title'>
-          <span>Home</span>
-        </h3>
-        <p>This is a react case study project.</p>
-      </div>
-    )
-  }
+export default function Home() {
+  const myFunction = (min, max) => {
+    const RND = min + Math.floor(Math.random() * (max - min));
+
+    console.log(RND);
+
+    const sum = (x, y) => {
+      return x * y + RND;
+    };
+
+    console.log(sum(5, 2));
+
+    return sum;
+  };
+
+  const test = () => {
+    const result = myFunction(25, 50);
+    console.log(result);
+
+    console.log(result(5, 2));
+    console.log(result(3, 4));
+  };
+
+  const counter = (initCounter) => {
+    let counter = initCounter;
+
+    const increment = () => {
+      counter++;
+      return counter;
+    };
+
+    const decrement = () => {
+      counter--;
+      return counter;
+    };
+
+    return { increment, decrement };
+  };
+
+  const myCounter = counter(25);
+  console.log(myCounter);
+
+  const { increment, decrement } = myCounter;
+
+  const incrementCounterHandler = () => {
+    let c = increment();
+    console.log(c);
+  };
+  const decrementCounterHandler = () => {
+    let c = decrement();
+    console.log(c);
+  };
+  return (
+    <div className={['container', 'mt-5'].join(' ')}>
+      <h3 className='title'>
+        <span>Home</span>
+      </h3>
+      <button onClick={test}>Test</button>
+      <button onClick={incrementCounterHandler}>Increment</button>
+      <button onClick={decrementCounterHandler}>Decrement</button>
+      <p>This is a react case study project.</p>
+    </div>
+  );
 }
